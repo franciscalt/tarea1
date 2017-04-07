2d79790d12f5a5e0e2d0c461b06c4aa1b3dac6ae
francisca Lathrop <franciscalathrop@MacBook-Air-de-Fran.local>
2017-04-07 16:26:06 -0300

    nuevo

---
+++ main.js
@@ -0,0 +1,142 @@
+var dia = prompt("ingresa el dia de tu cumpleaños")
+var mes = prompt("ingresa el mes de tu cumpleaños")
+var a = prompt("ingresa el año de tu cumpleaños")
+
+// // var ene = 'enero'
+// // var feb = 'febrero'
+// // var mar = 'marzo'
+// // var abr = 'abril'
+// // var may = 'mayo'
+// // var jun = 'junio'
+// // var jul = 'julio'
+// // var ago = 'agosto'
+// // var sep = ' agosto'
+
+var ene = 'enero'
+var feb = 'febrero'
+var mar = 'marzo'
+var abr = 'abril'
+var may = 'mayo'
+var jun = 'junio'
+var jul = 'julio'
+var ago = 'agosto'
+var sep = 'septiembre'
+var oct = 'octubre'
+var nov = 'noviembre'
+var dic = 'diciembre'
+
+
+var cumpleenero = [ 'febrero' , 'marzo' ,  'abril' , 'mayo' , 'junio' , 'julio' , 'agosto' , 'septiembre'  , oct , nov , dic] 
+var cumplefebrero = ['enero'  , 'marzo' ,  'abril' , 'mayo' , 'junio' , 'julio' , 'agosto' , 'septiembre'  , oct , nov , dic] 
+var cumplemarzo = ['enero' , 'febrero'  ,  'abril' , 'mayo' , 'junio' , 'julio' , 'agosto' , 'septiembre'  , oct , nov , dic] 
+var cumpleabril = ['enero' , 'febrero' , 'marzo' , 'mayo' , 'junio' , 'julio' , 'agosto' , 'septiembre'  , oct , nov , dic] 
+var cumplemayo = ['enero' , 'febrero' , 'marzo' ,  'abril'  , 'junio' , 'julio' , 'agosto' , 'septiembre'  , oct , nov , dic] 
+var cumplejunio = ['enero' , 'febrero' , 'marzo' ,  'abril' , 'mayo'  , 'julio' , 'agosto' , 'septiembre'  , oct , nov , dic] 
+var cumplejulio = ['enero' , 'febrero' , 'marzo' ,  'abril' , 'mayo' , 'junio'  , 'agosto' , 'septiembre'  , oct , nov , dic] 
+var cumpleagosto = ['enero' , 'febrero' , 'marzo' ,  'abril' , 'mayo' , 'junio' , 'julio'  , 'septiembre'  , oct , nov , dic] 
+var cumpleseptiembre = ['enero' , 'febrero' , 'marzo' ,  'abril' , 'mayo' , 'junio' , 'julio' , 'agosto'   , oct , nov , dic] 
+var cumpleoctubre = ['enero' , 'febrero' , 'marzo' ,  'abril' , 'mayo' , 'junio' , 'julio' , 'agosto' , 'septiembre'   , nov , dic] 
+var cumplenoviembre = ['enero' , 'febrero' , 'marzo' ,  'abril' , 'mayo' , 'junio' , 'julio' , 'agosto' , 'septiembre'  , oct , dic] 
+var cumplediciembre = ['enero' , 'febrero' , 'marzo' ,  'abril' , 'mayo' , 'junio' , 'julio' , 'agosto' , 'septiembre'  , oct , nov ]  
+
+// var i = 0
+//  if( i = 0){ 
+//    console.log(cumpleenero)
+//  }
+
+// var i = 0
+//  if( i = 1){ 
+//    console.log(cumplefebrero)
+//  }
+
+// var i = 0
+//  if( i = 2){ 
+//    console.log(cumplemarzo)
+//  }
+
+// var i = 0
+//  if( i = 3){ 
+//    console.log(cumpleabril)
+//  }
+
+// var i = 0
+//  if( i = 4){ 
+//    console.log(cumpleenero)
+//  }
+ 
+// var i = 0
+//  if( i = 5){ 
+//    console.log(cumplefebrero)
+//  }
+
+// var i = 0
+//  if( i = 6){ 
+//    console.log(cumplemarzo)
+//  }
+
+// var i = 0
+//  if( i = 7){ 
+//    console.log(cumpleabril)
+//  }
+
+// var i = 0
+//  if( i = 8){ 
+//    console.log(cumpleenero)
+//  }
+ 
+// var i = 0
+//  if( i = 9){ 
+//    console.log(cumplefebrero)
+//  }
+
+// var i = 0
+//  if( i = 10){ 
+//    console.log(cumplemarzo)
+//  }
+
+// var i = 0
+//  if( i = 11){ 
+//    console.log(cumpleabril)
+//  }
+
+
+
+
+
+if(i = 0){
+console.log(cumpleenero)
+}
+else{ if(i = 1){
+console.log(cumplefebrero)
+}}
+if(i = 2){
+console.log(cumplemarzo)
+}
+
+if(i = 3){
+console.log(cumpleabril)
+}
+if(i = 4 ){
+console.log(cumplemayo)
+}
+if(i = 5){
+console.log(cumplejunio)
+}
+if(i = 6){
+console.log(cumplejulio)
+}
+if(i= 7){
+console.log(cumpleagosto)
+}
+if(i= 8){
+console.log(cumpleseptiembre)
+}
+if(i = 9){
+console.log(cumpleoctubre)
+}
+if(i = 10){
+console.log(cumplenoviembre)
+}
+if(i= 11){
+console.log(cumplediciembre)
+}
